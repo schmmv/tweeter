3d2766798ebc73538dce929521c55b36a4a48f51
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-01-24 20:12:19 +0000

    Change function to be anonymous and inline to click even

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  
-  const clickHandler = function() {
+ 
+  $('#tweet-text').on('input', function() {
     const maxLength = 140;
 
     let remainingChar = maxLength - $(this).val().length;
@@ -11,17 +11,6 @@ $(document).ready(function() {
     } else {
       counter.removeClass('counter-err');
     }
-  }
-
-  $('#tweet-text').on('input', clickHandler);//counts delete and space
+  });
 });
 
-
-// const clickHandler1 = function(event) {
-  //   console.log(event.type);
-  // }
-  // $('#tweet-text').keydown(clickHandler1);//counts shift, space, delete
-  // $('#tweet-text').keyup(clickHandler1);//counts shift, space, delete
-  // $('#tweet-text').keypress(clickHandler1); //counts the space bar
-    // $('#tweet-text').change(clickHandler1);
-    // $('#tweet-text').blur(clickHandler1);
