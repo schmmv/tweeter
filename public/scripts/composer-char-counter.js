ab2e292596559a1f1c42b57a37c469a248c78e76
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-01-24 20:47:03 +0000

    Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,23 @@
+/**
+ * Calculate number of characters in tweet textarea, and live-update the counter
+ * If counter exceeds max characters, style counter differently
+ */
+
 $(document).ready(function() {
  
   $('#tweet-text').on('input', function() {
     const maxLength = 140;
-
     let remainingChar = maxLength - $(this).val().length;
     let counter = $(this).parents().find('.counter');
+
     counter.text(remainingChar);
+
     if (counter.text() < 0) {
       counter.addClass('counter-err');
     } else {
       counter.removeClass('counter-err');
     }
+    
   });
 });
 
