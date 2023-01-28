a71bd4b7ea3c63275f4a4ec147bb1e6a920fe90e
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-01-28 16:48:47 +0000

    Enable text area automatically with .focus()

--- public/scripts/write-new-tweet.js
+++ public/scripts/write-new-tweet.js
@@ -1,12 +1,14 @@
 /**
  * Make new tweet form visible upon clicking on Write a new tweet arrow icon
  * Also scroll down to the form
+ * Also Enable textarea automatically
  */
 
 $(document).ready(function() {
 
   $('.nav-right-btn').click(function() {
     $('#tweet-form').slideToggle(500);
+    $('#tweet-text').focus();
     $('html, body').animate({
       scrollTop: $('#tweet-form').offset().top
     }, 1000);
