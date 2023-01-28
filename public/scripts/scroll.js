31b7bb629be98ff861bd1f0ea16deb65a5d81184
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-01-28 22:11:40 +0000

    Add scroll to top button and logic

---
+++ public/scripts/scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  const btn = $('#scroll-btn');
+  $(window).on('scroll', function() {
+    if($(window).scrollTop() > 300) {
+      btn.css({ display: 'block' });
+    
+  } else {
+    btn.css({ display: 'none' });
+  }});
+
+  btn.click(function() {
+    $(window).scrollTop(0);
+  });
+
+})
